c91564f99efb770ae89b91548e4aa182a53126e4
Borodun <a.borodin2@g.nsu.ru>
2021-02-04 15:04:08 +0700

    Word Statistic

---
+++ Lab1/src/main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.print("Write at least two arguments (e.g. '../input.txt ../output.csv')");
+        } else {
+            WordStatistic wordStatistic = new WordStatistic();
+            try {
+                for (int i = 0; i <= args.length - 2; ++i) {
+                    wordStatistic.addFile(args[i]);
+                }
+                wordStatistic.writeCSV(args[args.length - 1]);
+            }catch (IOException exception){
+                System.err.print("Exception: " + exception.getLocalizedMessage());
+            }
+        }
+    }
+}
